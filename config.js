a4001d4f258823ddbedcc37d7ad7622371a6b859
acwyprovjp <86775658+acwyprovjp@users.noreply.github.com>
2021-07-01 21:45:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-    introTitle: 'Ly à!',
+    introTitle: 'Nhật anh à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'oh my god!!!',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
@@ -15,6 +15,6 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Chúng ta bắt đầu chứ??.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/tit.tuan.58726' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100050238801994' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
     
