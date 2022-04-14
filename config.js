4e9bf60ca6265a8ae487df38d6e944c4b01ff4b3
acwyprovjp <86775658+acwyprovjp@users.noreply.github.com>
2022-04-14 16:42:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Chào em  !',
+    introTitle: 'Chào Nhật Anh  !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
